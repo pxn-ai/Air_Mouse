62e80dc26e7025a3c1c5d44f7128d40cc5b31fc5
pasan_r_k_95 <kaluarachchikapr.24@uom.lk>
2026-01-20 19:31:08 +0530

    i2c checker code

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,51 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_NeoPixel.h>
-#include "USB.h"
-#include "USBHIDMouse.h"
 
-// --- Hardware Config ---
-// CHECK YOUR PINOUT: S3 boards vary wildy!
-// Common S3 DevKit: SDA=8, SCL=9, NeoPixel=48
-// S3 Zero: SDA=5, SCL=6, NeoPixel=21
-#define SDA_PIN 8
-#define SCL_PIN 9
-#define NEOPIXEL_PIN 48 
-
-USBHIDMouse Mouse;
-Adafruit_MPU6050 mpu;
-Adafruit_NeoPixel pixels(1, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
-// --- Mouse Tuning ---
-const float GYRO_DEADZONE = 0.15;  // Rad/s
-const float MOUSE_SENSITIVITY = 18.0; 
-const float CLICK_THRESHOLD = 15.0; // Acceleration m/s^2 for shake click
-
-// Timer for shake debounce
-unsigned long lastClickTime = 0;
+// specific pins for your ESP32-S3 setup
+#define I2C_SDA_PIN 1
+#define I2C_SCL_PIN 2
 
 void setup() {
-    // Start Native USB
-    USB.begin();
-    Mouse.begin();
-    Serial.begin(115200);
-
-    // Init NeoPixel
-    pixels.begin();
-    pixels.setBrightness(30);
-    pixels.setPixelColor(0, pixels.Color(255, 100, 0)); // Orange = Booting
-    pixels.show();
-
-    // Init I2C
-    Wire.begin(SDA_PIN, SCL_PIN);
-
-    // Init MPU6050
-    if (!mpu.begin()) {
-        Serial.println("MPU6050 Not Found!");
-        while (1) {
-            pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // Red = Error
-            pixels.show();
-            delay(100);
-            pixels.setPixelColor(0, 0);
-            pixels.show();
-            delay(100);
-        }
-    }
-
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-
-    pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // Green = Ready
-    pixels.show();
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+  Serial.begin(115200);
+  while (!Serial); // Wait for serial monitor
+  Serial.println("\nI2C Scanner");
 }
 
 void loop() {
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
+  byte error, address;
+  int nDevices;
 
-    // --- 1. Mouse Movement (Gyro) ---
-    // MPU6050 Orientation mapping - Change signs/axes based on how you hold it
-    float gyroZ = g.gyro.z; // Yaw (Left/Right)
-    float gyroY = g.gyro.y; // Pitch (Up/Down)
+  Serial.println("Scanning...");
 
-    int moveX = 0;
-    int moveY = 0;
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
 
-    if (abs(gyroZ) > GYRO_DEADZONE) {
-        moveX = (int)(gyroZ * MOUSE_SENSITIVITY * -1);
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) 
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println("  !");
+
+      nDevices++;
     }
-    if (abs(gyroY) > GYRO_DEADZONE) {
-        moveY = (int)(gyroY * MOUSE_SENSITIVITY * -1);
-    }
+    else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16) 
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
 
-    if (moveX != 0 || moveY != 0) {
-        Mouse.move(moveX, moveY);
-    }
-
-    // --- 2. Shake to Click (Accelerometer) ---
-    // If total acceleration vector is high, it's a shake
-    // Gravity is ~9.8, so we look for spikes well above that
-    float totalAccel = sqrt(pow(a.acceleration.x, 2) + 
-                            pow(a.acceleration.y, 2) + 
-                            pow(a.acceleration.z, 2));
-
-    if (totalAccel > CLICK_THRESHOLD && millis() - lastClickTime > 500) {
-        Mouse.click(MOUSE_LEFT);
-        lastClickTime = millis();
-        
-        // Visual Feedback for Click (White Flash)
-        pixels.setPixelColor(0, pixels.Color(255, 255, 255));
-        pixels.show();
-        delay(50); 
-        pixels.setPixelColor(0, pixels.Color(0, 255, 0));
-        pixels.show();
-    }
+  delay(5000); // Wait 5 seconds for next scan
 }
